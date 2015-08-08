3e59866dafc641bada7745e5547e1759912415d8
Gary Dohmeier <gary.dohmeier@avnet.com>
2015-08-08 09:51:22 -0400

    first

---
+++ learningchef/chap03/hello.rb
@@ -0,0 +1,6 @@
+
+file 'hello.txt' do
+  content 'Welcome to Chef'
+end
+
+#Excerpt From: Mischa Taylor and Seth Vargo. “Learning Chef.” iBooks. 
---
+++ learningchef/chap03/stone.rb
@@ -0,0 +1,8 @@
+
+
+file "#{ENV['HOME']}/stone.txt" do
+  content 'Written in stone'
+end
+
+
+#Excerpt From: Mischa Taylor and Seth Vargo. “Learning Chef.” iBooks. 
